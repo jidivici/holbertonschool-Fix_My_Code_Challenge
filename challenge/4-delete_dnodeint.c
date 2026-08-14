9156b45d624cee2a2cc9e1034db8e5b409b8b355
__fdbrv <brice.fontaine.83@icloud.com>
2026-08-14 21:22:23 +0200

    Fix: (*head)->prev->next = (*head)->next;

--- challenge/4-delete_dnodeint.c
+++ challenge/4-delete_dnodeint.c
@@ -11,42 +11,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	dlistint_t *previous;
-	dlistint_t *next;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
 	unsigned int p;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 	{
 		return (-1);
 	}
-	current = *head;
+	saved_head = *head;
 	p = 0;
-	while (p < index && current != NULL)
+	while (p < index && *head != NULL)
 	{
-		current = current->next;
+		*head = (*head)->next;
 		p++;
 	}
 	if (p != index)
 	{
+		*head = saved_head;
 		return (-1);
 	}
-
-	previous = current->prev;
-	next = current->next;
-	if (previous != NULL)
+	if (0 == index)
 	{
-		previous->next = next;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
 	}
 	else
 	{
-		*head = next;
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
 	}
-	if (next != NULL)
-	{
-		next->prev = previous;
-	}
-	free(current);
 
 	return (1);
 }
